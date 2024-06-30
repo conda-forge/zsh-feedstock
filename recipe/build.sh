5bacaa39f21b9d12843922fb62975e214fbfa874
Matthias Diener <mdiener@illinois.edu>
2024-06-30 08:28:52 -0500

    Cleanups

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,12 +8,25 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 # Work around https://github.com/Homebrew/homebrew-core/pull/64923
 export CFLAGS+=" -Wno-implicit-function-declaration -Wno-implicit-int"
 
-./configure --prefix="$PREFIX" --with-tcsetpgrp --enable-pcre
+./configure --prefix="$PREFIX" \
+    --enable-cap \
+    --enable-maildir-support \
+    --enable-multibyte \
+    --enable-pcre \
+    --enable-zsh-secure-free \
+    --enable-etcdir=/etc \
+    --with-tcsetpgrp \
+    DL_EXT=bundle \
 
-cat config.log
 
 make
+
+if [[ $target_platform == osx-* ]]; then
+    # This tests fails on MacOS
+    rm Test/A04redirect.ztst
+fi
 
 [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]] && make check
 
 make install
+make install.info
